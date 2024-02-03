ab39e0b513d4bc22d550fd4563a593fa159d4bdd
Pegassuo <carlos.xbr54@gmail.com>
2024-02-02 20:08:19 -0500

    Activity: Contacts Class

---
+++ app/src/main/java/com/example/falldetection/presentation/ContactsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.falldetection.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.falldetection.R
+
+class ContactsActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.contacts)
+    }
+}
