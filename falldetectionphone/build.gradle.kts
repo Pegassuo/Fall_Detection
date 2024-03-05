6285aa7a2c910785a50aaa8c6cf48998a58a5e96
Pegassuo <carlos.xbr54@gmail.com>
2024-03-04 19:09:29 -0500

    Update project configurations

--- falldetectionphone/build.gradle.kts
+++ falldetectionphone/build.gradle.kts
@@ -54,4 +54,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("com.google.android.gms:play-services-wearable:18.1.0")
+    implementation("com.google.code.gson:gson:2.8.8")
 }
